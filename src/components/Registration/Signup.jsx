c1d263e85ef7c6905e0586285316dc0d7e39ff7e
Olukayode <olukayodeazeez2000@gmail.com>
2024-04-13 08:57:25 +0100

    Mandate

--- src/components/Registration/Signup.jsx
+++ src/components/Registration/Signup.jsx
@@ -17,10 +17,12 @@ const Signup = () => {
 
   // Validate Password
   const validatePassword = (password) => {
-    const passwordRegex =
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&/])[A-Za-z\d@$!%*?&/]{8,}$/
     return passwordRegex.test(password)
   }
+
+
 
   // Validate Email
   const validateEmail = (email) => {
